c8170a72199870c8505ec1b8998918c6716a87d4
AnzhelaAyd <angiemagomedova@yandex.com>
2024-08-19 14:15:37 +0300

    Update module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -1,13 +1,22 @@
-a = int(input('введи число'))
+numbers=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes=[]
+not_primes=[]
 
-k=0
-for i in range(2,16):
-    if list(a) % i ==0:
-        k=k+1
-if (k<=0):
-    print('Число простое')
-else:
-    print('Число составное')
+for i in numbers:
+    k = 0
+    if i == 1:
+        continue
+    for j in range(2, i):
+        if i % j ==0:
+            k=k+1
+            break
+    if k<=0 :
+        primes.append(i)
+    else:
+        not_primes.append(i)
+print('Простые числа', primes)
+print('Составные числа', not_primes)
+
 
 
 
